046472c4f3f9539ed7a38a15ef2c3198df57d4d7
Muhammed Buhari Horoz <buhari.horoz@ug.bilkent.edu.tr>
2025-04-27 15:44:39 +0300

    ShoppingCart Class template

--- src/virtualMarket/customer/ShoppingCart.java
+++ src/virtualMarket/customer/ShoppingCart.java
@@ -1,5 +1,26 @@
 package virtualMarket.customer;
 
+import java.util.ArrayList;
+import virtualMarket.order.*;
+import virtualMarket.items.*;
+
 public class ShoppingCart {
+	private Customer customer;
+	private ArrayList<Item> items;
+
+	public ShoppingCart(Customer customer) {
+		super();
+		this.customer = customer;
+	};
+
+	public void addItem(Item item) {
+	};
+
+	public void removeItem(Item item) {
+	};
+
+	public Order checkOut() {
+		return null;
+	};
 
 }
